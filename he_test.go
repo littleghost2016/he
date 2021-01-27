91d621684e5879340ff512bbd0103da2f62fdbbe
littleghost2016 <785340571@qq.com>
2021-01-27 16:42:07 +0800

    更新：使用benchmark进行时间测试

--- he_test.go
+++ he_test.go
@@ -1,22 +1,27 @@
 package he
 
 import (
-	"fmt"
 	"math/big"
 	"testing"
-	"time"
 
 	"github.com/bwesterb/go-ristretto"
 )
 
-func TestXN(t *testing.T) {
+func BenchmarkXN(b *testing.B) {
 
+	for i := 0; i < b.N; i++ {
+		ttt()
+	}
+
+}
+
+func ttt() {
 	xNString := []string{
-		"1",
+		// "1",
 		"444444444444",
 		"2037035976334486086268445688409378161051468393665936250636140449354381299763336706183397376",
-		"2",
-		"34234",
+		// "2",
+		// "34234",
 	}
 	// 给定一组x的值，每个值为字符串类型
 
@@ -41,24 +46,27 @@ func TestXN(t *testing.T) {
 
 		cN[i] = CommitTo(H, rN[i], new(ristretto.Scalar).SetBigInt(xN[i]))
 		// 通过CommitTo函数进行计算密文C
-		fmt.Println("xN[i]: ", xN[i], "rN[i]: ", rN[i], "cN[i]: ", cN[i])
+		// fmt.Println("xN[i]: ", xN[i], "rN[i]: ", rN[i], "cN[i]: ", cN[i])
 	}
 
-	C1 := AddMoreThanTwo(cN)
+	// C1 := AddMoreThanTwo(cN)
+	_ = AddMoreThanTwo(cN)
 	// 密文相加（先加密后相加）
 
-	startTime := time.Now().UnixNano()
-	C2 := AddPrivatelyMoreThanTwo(H, rN, xN)
+	// startTime := time.Now().UnixNano()
+	// C2 := AddPrivatelyMoreThanTwo(H, rN, xN)
+	_ = AddPrivatelyMoreThanTwo(H, rN, xN)
 	// 参数相加（先相加后加密）
 
-	fmt.Println("C1: ", C1, "C2: ", C2)
-	if C2.Equals(C1) {
-		// 判断计算结果是否相等
-		fmt.Println("equal")
-	} else {
-		fmt.Println("not equal")
-	}
+	// fmt.Println("C1: ", C1, "C2: ", C2)
+	// if C2.Equals(C1) {
+	// 	// 判断计算结果是否相等
+	// 	fmt.Println("equal")
 
-	endTime := time.Now().UnixNano()
-	fmt.Println("第二个耗时：（纳秒）", endTime-startTime)
+	// } else {
+	// 	fmt.Println("not equal")
+	// }
+
+	// endTime := time.Now().UnixNano()
+	// fmt.Println("第二个耗时：（纳秒）", endTime-startTime)
 }
